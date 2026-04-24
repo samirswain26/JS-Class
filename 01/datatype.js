2d0f0c953481a81fdfde459dccc946add2838376
Samir Kumar Swain <samirkumarswain267@gmail.com>
2026-04-24 18:55:05 +0530

    Explained about the data types in js

---
+++ 01/datatype.js
@@ -0,0 +1,33 @@
+// Number
+let a = 43
+const b = 21
+a =111
+
+// String
+const s = "Samir"
+let d = "Kumar"
+
+// Number
+let age = 23
+
+// Boolean - true , false
+let student = true
+
+// null
+let favoriteFood = null
+
+// Undefined
+let FavPlace = undefined
+
+// Array
+
+let array = [ "a", "b", 'c' ] 
+// [] 
+// ()
+//  {} 
+// array with the data types
+// [ "a", "b", 'c' ] --> [ 'a', 'b', 'c' ]
+
+console.log(array)
+
+console.log(a)
---
+++ 01/index.js
@@ -0,0 +1,13 @@
+const a = 5
+
+function login (){
+    return a
+}
+
+console.log(login())
+
+const b = async () => {
+    console.log("ABCD")
+}
+
+b()
