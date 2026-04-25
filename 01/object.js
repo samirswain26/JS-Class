f94cd5e2cba1e09bf01e4f46e24abdac2cc9e60f
Samir Kumar Swain <samirkumarswain267@gmail.com>
2026-04-25 10:59:43 +0530

    A fresh intro about Array and object

---
+++ 01/object.js
@@ -0,0 +1,36 @@
+let a = {
+  name: "Samir Kumar Swain",
+  age: 23,
+  description: "Developer",
+  isStudent: true,
+};
+
+// console.log(a.age);
+
+const arr = [
+  {
+    name: "Samir Kumar Swain",
+    age: 23,
+    description: "Developer",
+    isStudent: true,
+  },
+  {
+    name: "Samir Kumar Swain",
+    age: 23,
+    description: "Developer",
+    isStudent: true,
+  },
+  {
+    name: "Samir Kumar Swain",
+    age: 23,
+    description: "Developer",
+    isStudent: true,
+  },
+];
+
+let arr1 = [[[],[]],[],[],[]]
+
+// function a (){}
+// [] --> map, filter,
+// console.log(arr.map(() => {}))
+
